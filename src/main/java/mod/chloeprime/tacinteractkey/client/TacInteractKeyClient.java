acc0218af9ad401e6bedc2fded28faf4bbe84314
Locus_Natit <3113135718@qq.com>
2024-02-10 21:50:31 +0800

    Compat to TaC 0.3.8

--- src/main/java/mod/chloeprime/tacinteractkey/client/TacInteractKeyClient.java
+++ src/main/java/mod/chloeprime/tacinteractkey/client/TacInteractKeyClient.java
@@ -1,6 +1,7 @@
 package mod.chloeprime.tacinteractkey.client;
 
 import com.tac.guns.client.GunConflictContext;
+import com.tac.guns.client.settings.TacKeyBingding;
 import net.minecraft.client.settings.KeyBinding;
 import net.minecraft.client.util.InputMappings;
 import net.minecraftforge.fml.client.registry.ClientRegistry;
@@ -9,9 +10,16 @@ import org.lwjgl.glfw.GLFW;
 
 public class TacInteractKeyClient
 {
-    public static final KeyBinding KEY_GUN_INTERACT = new KeyBinding(
-            "key.tacinteractkey.interact", GunConflictContext.IN_GAME_HOLDING_WEAPON,
-            InputMappings.Type.KEYSYM, GLFW.GLFW_KEY_F, "key.categories.inventory"
+//    public static final KeyBinding KEY_GUN_INTERACT = new KeyBinding(
+//            "key.tacinteractkey.interact", GunConflictContext.IN_GAME_HOLDING_WEAPON,
+//            InputMappings.Type.KEYSYM, GLFW.GLFW_KEY_F, "key.categories.inventory"
+//    );
+    // Use TacKeyBingding to avoid duplicate key respond
+    // In order to compat with fxxking TaC 0.3.8's mixin
+    // In other words, it's more recommended to use codes
+    // above when TaC is not in 0.3.8
+    public static final TacKeyBingding KEY_GUN_INTERACT = new TacKeyBingding(
+            "key.tacinteractkey.interact",  GLFW.GLFW_KEY_F, "key.categories.inventory", false
     );
 
     public static void construct()
